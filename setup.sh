efbdce133824546503dc86b5fc1e38750586bb17
jeffwcollins <jeffwcollins@gmail.com>
2019-05-15 13:06:12 -0500

    cleaned up script

--- setup.sh
+++ setup.sh
@@ -1,18 +1,2 @@
-cp elastic*.list /etc/apt/sources.list.d/
-&
-cp jonathonf*.list /etc/apt/sources.list.d/
-&
-cp sources.list /etc/apt/
-&
-apt-get update & apt-get upgrade -y
-&
-sleep 90s
-&
-apt-get install landscape-client -y
-&
-sleep 90s
-&
-cp landscape_server_ca.crt /etc/landscape/
-&
-cp client.conf /etc/landscape/
+cp elastic*.list /etc/apt/sources.list.d/ & cp jonathonf*.list /etc/apt/sources.list.d/ & cp sources.list /etc/apt/ & apt-get update & apt-get upgrade -y & sleep 90s & apt-get install landscape-client -y & sleep 90s & cp landscape_server_ca.crt /etc/landscape/ & cp client.conf /etc/landscape/
 
