4494c5de23b6047ba2d3d62b5c0318eeb465d23a
jeffwcollins <jeffwcollins@users.noreply.github.com>
2019-05-15 13:15:38 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,11 @@
-cp elastic*.list /etc/apt/sources.list.d/ & cp jonathonf*.list /etc/apt/sources.list.d/ & cp sources.list /etc/apt/ & apt-get update & apt-get upgrade -y & sleep 90s & apt-get install landscape-client -y & sleep 90s & cp landscape_server_ca.crt /etc/landscape/ & cp client.conf /etc/landscape/
-
+cp elastic*.list /etc/apt/sources.list.d/
+sleep 2
+cp jonathonf*.list /etc/apt/sources.list.d/
+sleep 2
+cp sources.list /etc/apt/
+sleep 2
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sleep 2
+apt-get update & apt-get upgrade -y
+sleep 90s
+apt-get install landscape-client -y & sleep 90s & cp landscape_server_ca.crt /etc/landscape/ & cp client.conf /etc/landscape/
